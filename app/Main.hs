0ae885e6ccc605c8140ecd4e0cde3b3fc036e082
slothman3878 <sloth.man3878@gmail.com>
2021-10-23 18:32:48 -0400

    ver2

--- app/Main.hs
+++ app/Main.hs
@@ -8,44 +8,43 @@ import Data.List.Split
 main :: IO ()
 main = do
   -- read the raw .bmp file
-  raw <- BS.readFile "MARBLES.BMP"
+  raw <- BS.readFile "new_bmp.bmp"
   let content = BS.unpack raw
   -- get the index where pixels start
   let offSet = toInt [content!!10, content!!11, content!!12, content!!13]
   -- get header bytes
-  let header = take offSet content
-  let pixelData = drop offSet content
+  let (header,pixelData) = splitAt offSet content
   -- apply constraints on header
 
   -- get image width
   let width = toInt [content!!18, content!!19, content!!20, content!!21]
-  print width
+  -- get rowSize of pixelData
   let rowSize = (*) 4 $ ceiling $ (24 * (fromIntegral width)) / 32
-  print rowSize
-  -- negate pixels
-  let negativePixels = map negatePixel (toPixels rowSize width pixelData)
+  -- parse pixel data and get negative colors
+  let negativePixels = toByteArray $ negateImage $ parseImage width rowSize pixelData
   -- write to bmp file
   BS.writeFile "new_bmp.bmp" $ BS.pack $ header ++ negativePixels
   print "success!"
 
 -- HexNum to Int for parsing Header
 toInt :: [Word8] -> Int
+toInt [] = 0
 toInt [x] = fromIntegral x
 toInt (x:xs) = fromIntegral x + (shift (toInt xs) 8)
 
--- Given rowSize and width, return parsed Pixels
-toPixels :: Int -> Int -> [Word8] -> Pixels
-toPixels rowSize width pixels = concat (map (parseRow width) (chunksOf rowSize pixels))
+-- Parsing Image given image width and pixel data row size
+type Byte = Word8
+type BmpImage = [([Byte],[Byte])] --[([RGB Byte],[Padding Byte])]
 
--- Parsing a Single Row
-parseRow :: Int -> [Word8] -> Pixels
-parseRow width = (\(x,y) -> map RGB x ++ (map (\z->Padding) y)) . (splitAt $ 4*width)
+-- Each row is rowSize bytes long
+-- For each row, the padding is the last (rowSize - 3*width) bytes
+parseImage :: Int -> Int -> [Word8] -> BmpImage
+parseImage width rowSize = map (splitAt $3*width) . chunksOf rowSize
 
-negatePixel :: MaybeColor -> Word8
-negatePixel pixel = case pixel of
-  RGB a -> 255 - a
-  Padding -> 0
+-- simply concatenate everything
+toByteArray :: BmpImage -> [Word8]
+toByteArray = concat . map (\(x,y)-> x++y)
 
--- A byte in the pixel data of a bmp is either an RGB value or a Padding
-data MaybeColor = RGB Word8 | Padding
-type Pixels = [MaybeColor]
+-- Negating the RGB values
+negateImage :: BmpImage -> BmpImage
+negateImage = map (\(x,y)-> (map ((-) 255) x , y))
